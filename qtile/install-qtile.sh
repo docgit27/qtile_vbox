48798c85228328b6de965d79fec249da0e028416
docgit27 <davivo005@gmail.com>
2024-12-31 23:07:15 +0100

    updating config.py file with new widget settings

--- qtile/install-qtile.sh
+++ qtile/install-qtile.sh
@@ -1,21 +1,25 @@
-#!/bin/bash/
+##!/bin/bash/
 #updating
 sudo apt update
 
 #installing everything i need for qtile
-sudo apt install python3 python3-pip python3-venv python3-v-sim python-dbus-dev libpangocairo-1.0-0 python3-xcffib python3-cairocffi libxkbcommon-dev libxkbcommon-x11-dev -yy
+sudo apt install python3 python3-pip python3-venv python3-v-sim python-dbus-dev libpangocairo-1.0-0 python3-xcffib python3-cairocffi libxkbcommon-dev libxkbcommon-x11-dev pulseaudio -yy
 
-#creating a virtual environment for qtile
+#creating a virtual environment for qtile, pulsectl and pulsectl-asyncio
 mkdir -p ~/.local/src/
 cd ~/.local/src/
 python3 -m venv qtile_venv
 
-#cloning qtile from git
+#cloning qtile, pulsectl and pulsectl-asyncio from git
 cd qtile_venv/
 git clone https://github.com/qtile/qtile.git
+git clone https://github.com/mk-fg/python-pulse-control.git
+git clone https://github.com/mhthies/pulsectl-asyncio.git
 
-#installing qtile
+#installing qtile, pulsectl and pulsectl-asyncio
 bin/pip install qtile/
+bin/pip install pulsectl/
+bin/pip install pulsectl_asyncio/
 
 #copying qtile binary file into ~/.local/bin
 mkdir ~/.local/bin/
